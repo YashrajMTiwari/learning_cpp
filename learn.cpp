2a1ec61bf1be6ac8f78344e9801054153f9776d2
YashrajMTiwari <yashrajtiwari4362@gmail.com>
2025-11-23 19:54:22 +0530

    Learned about -Wall: Fro debugging, -O0,2,3 fro optimization, -Os for small binary, -Ofast for fast binary

--- learn.cpp
+++ learn.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
 
-int main() {
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
-}
-
-
-// Old code for references
-/*
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int a = 30;
-int b = 20;
-int c = a + b;
-int * pX = &a;
 
 int main() {
-    cout << "Addition of  " << a << " + " << b << " is: " << c << endl;
-    cout << "End " << *pX - b;
-    return 0;
+    std::cout << "Hellow World" << std::endl;
 }
-*/
