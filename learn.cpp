7baa2334962e9afb70a8115a736b957d486bad13
YashrajMTiwari <yashrajtiwari4362@gmail.com>
2025-11-23 20:08:35 +0530

    Learned comments

--- learn.cpp
+++ learn.cpp
@@ -1,6 +1,12 @@
+// Learning c++ from basics
 #include <iostream>
 
-
 int main() {
-    std::cout << "Hellow World" << std::endl;
+    // This will print hello world
+    std::cout << "Hello! World" << std::endl;
+    /*
+    This is amulti line comment in c++
+    The line  return 0; indicates the program has ended successfully.
+    */
+    return 0;
 }
